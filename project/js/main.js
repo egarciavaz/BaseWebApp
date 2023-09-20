06c7428b93c25229b2acd6319e45500dc89e6f9a
Emilio Garcia <egarcia@boundify.io>
2023-09-19 22:51:40 -0400

    Added access to API

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+  getWeather();
+})
+
+function getWeather() {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=greenville,sc,us&appid="+apiKey;
+
+  $.ajax(url,{sucess: function(data){
+    console.log(data);
+  }})
+}
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
