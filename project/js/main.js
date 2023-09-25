2845b5f1eab04afd5e3100844e42e7a033c9c9cb
Emilio Garcia <egarcia@boundify.io>
2023-09-25 11:58:54 -0400

    Database APIs

--- project/js/main.js
+++ project/js/main.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
-  getWeather();
+  // getWeather();
 })
 
 function getWeather(searchQuery) {
   var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid="+apiKey;
+
+  $(".city").text("");
+  $(".temp").text("");
+  $(".error-message").text("");
 
   $.ajax(url,{success: function(data) {
     console.log(data);
     $(".city").text(data.name);
     $(".temp").text(data.main.temp);
   }, error: function(error) {
-    $("error-message").text("An error ocurred");
+    $(".error-message").text("An error ocurred");
   }})
 }
 
