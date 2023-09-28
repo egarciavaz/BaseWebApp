fc394a72b94962a09e9a78940e4e61b1a85d2f30
Emilio Garcia <egarcia@boundify.io>
2023-09-28 10:59:29 -0400

    This commit is a mistake

--- project/js/main.js
+++ project/js/main.js
@@ -2,31 +2,6 @@ $(document).ready(function() {
   // getWeather();
 })
 
-function handleSignIn() {
-  const provider = new GoogleAuthProvider();
-
-  const auth = getAuth();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-}
-
 function getWeather(searchQuery) {
   var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid="+apiKey;
 
